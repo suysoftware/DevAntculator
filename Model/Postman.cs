ff0147f46b27e3b879a65ad795c1deb5a0de7aa3
SUY <suy.register@gmail.com>
2021-07-24 10:10:12 +0300

    Add using statements and namespaces to Postman.cs

--- Model/Postman.cs
+++ Model/Postman.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using TezAPPV2;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Mime;
